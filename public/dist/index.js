7c09145e7a49420e254e0b445d8c48c189ac2577
Leticia Leal <leticia.leal@growdev.com.br>
2022-10-07 20:21:34 -0300

    regras

--- public/dist/index.js
+++ public/dist/index.js
@@ -2,3 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
 console.log(database_1.listaContas);
+// 1. Filtrar todas as operações de entrada maior ou igual a R$500.00 de cada conta do banco virtual
+// 2. Cadastrar uma nova conta bancária
+// 3. Adicionar uma nova transação de SAIDA bancária para a conta de número 112233 e Mostrar o saldo atual da conta 112233
+// 4. Deletar uma conta 
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import { listaContas } from './database';
 
 
 console.log(listaContas);
+
+// 1. Filtrar todas as operações de entrada maior ou igual a R$500.00 de cada conta do banco virtual
+// 2. Cadastrar uma nova conta bancária
+// 3. Adicionar uma nova transação de SAIDA bancária para a conta de número 112233 e Mostrar o saldo atual da conta 112233
+// 4. Deletar uma conta 
